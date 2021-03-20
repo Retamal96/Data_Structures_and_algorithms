003eb2c182b599bdf051c6113f95703d679e597f
Victor Retamal <victorrtml22@gmail.com>
2021-03-20 18:35:11 +0100

    Changes add to the maze solver
    Still needed to add the counter for min steps

--- Class_3/maze_solver/maze_solver.py
+++ Class_3/maze_solver/maze_solver.py
@@ -6,17 +6,11 @@ class Maze_solver():
         self.mapa = mapa 
         self.current_position = 'S'
         self.is_solve = False
+        self.current_row,self.current_column = self.find_start()
+        self.step_counter = []
+        self.counter = 0
+        self.short_path = 100000
    
-    def solve(self):
-        if self.current_position == 'E':
-            self.is_solve = True
-        
-        if not self.is_solve:
-            row,column = self.find_start()
-            #self.move()
-            #self.solve()
-
-
     def find_start(self):
         finded = False
         for row in range(len(self.mapa)):
@@ -24,13 +18,57 @@ class Maze_solver():
               if self.mapa[row][column] == 'S':
                   finded = True
                   return row,column  
-        if finded == False:
-            return 'Error'
+        raise 'No Start found'
+    
+    def can_move(self, current_row, current_column):
+        return 0<= current_row < len(self.mapa) and  \
+            0 <= current_column < len(self.mapa[0]) and \
+                (self.mapa[current_row][current_column] == ' ' or
+                self.mapa[current_row][current_column] == 'E')
 
+    def solve(self):
+        
+        if self.mapa[self.current_row][self.current_column] == 'E':
+            print(self)
+            self.step_counter.append(self.counter)
+            if self.short_path > self.counter:
+                self.short_path = self.counter
 
+        else:
+            #Position mark
+            self.mapa[self.current_row][self.current_column] = '#'
+            self.counter += 1
+
+            #up
+            if self.can_move(self.current_row - 1, self.current_column):
+                self.current_row -= 1
+                self.solve()
+                self.current_row += 1
+                
+            #down
+            if self.can_move(self.current_row + 1, self.current_column):
+                self.current_row += 1
+                self.solve()
+                self.current_row -= 1
+
+            #left
+            if self.can_move(self.current_row, self.current_column -1):
+                self.current_column -= 1
+                self.solve()
+                self.current_column += 1
+
+            #right
+            if self.can_move(self.current_row, self.current_column +1):
+                self.current_column += 1
+                self.solve()
+                self.current_column -= 1
+        
+        #undo postion marks
+        self.mapa[self.current_row][self.current_column] = ' '
+        self.counter -= 1
+
+    def shortest_path(self):
+        return min(self.step_counter)
 
     def __str__(self):
-        result = ""
-        for row in self.mapa:
-            result += str(row) + '\n'
-        return result
+        return "\n".join(str(row) for row in self.mapa) + "\n"
